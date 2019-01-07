2402627e3541072a47e67b034061d8afc008a53c
Bob den Os <bob.den.os@sap.com>
2019-01-07 13:32:36 +0100

    Improve test helper integration function coverage

--- core/def/test_test.go
+++ core/def/test_test.go
@@ -4,9 +4,84 @@ import (
 	"testing"
 )
 
+func checkServer() bool {
+	httpClient := HttpClient()
+	httpServer := HttpServer()
+
+	_, err := httpClient.Get(httpServer)
+
+	if err == nil {
+		return true
+	}
+	return false
+}
+
 func TestIntegration(t *testing.T) {
-	t.Run("Integraction()", func(t *testing.T) {
-		Integration()
+	t.Run("Integraction() => external", func(t *testing.T) {
+		ready = false
+
+		r := Integration()
+
+		if *integration {
+			if r {
+				return
+			}
+
+			if !checkServer() {
+				return
+			}
+
+			t.Error("Failed to determine correct integration state.", r)
+		}
+
+		if r {
+			t.Error("Failed to determine correct integration state.", r)
+		}
+	})
+
+	t.Run("Integraction() => forced false", func(t *testing.T) {
+		ready = false
+		orgIntegration := integration
+		defer func() { integration = orgIntegration }()
+
+		b := false
+		integration = &b
+
+		r := Integration()
+
+		if r {
+			t.Error("Failed to determine correct integration state.", r)
+		}
+	})
+
+	t.Run("Integraction() => forced true", func(t *testing.T) {
+		ready = false
+		orgIntegration := integration
+		defer func() { integration = orgIntegration }()
+
+		b := true
+		integration = &b
+
+		r := Integration()
+
+		if r && !checkServer() {
+			t.Error("Failed to determine correct integration state.", r)
+		}
+	})
+
+	t.Run("Integraction() => forced ready", func(t *testing.T) {
+		ready = true
+		orgIntegration := integration
+		defer func() { integration = orgIntegration }()
+
+		b := true
+		integration = &b
+
+		r := Integration()
+
+		if !r {
+			t.Error("Failed to determine correct integration state.", r)
+		}
 	})
 }
 
